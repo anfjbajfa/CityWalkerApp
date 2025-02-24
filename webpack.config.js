8613af379dd5663938e46524ba10450eb17e635f
Yu <fyu3228@gmail.com>
2025-02-24 13:29:29 -0600

    update login

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,27 @@
-// webpack.config.js
 const path = require('path');
 
 module.exports = {
-  mode: 'development', // or 'production'
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: 'https://anfjbajfa.github.io/CityWalkerApp/',
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader'
+        test: /\.js$/, 
+        exclude: /node_modules/, 
+        use: {
+          loader: 'babel-loader', 
+        },
       },
     ],
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    hot: true,
-    compress: true,
-    port: 3000,
-    historyApiFallback: true, 
+    contentBase: path.join(__dirname, 'dist'), 
+    hot: true, 
+    compress: true, 
+    port: 3000, 
   },
 };
