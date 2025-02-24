87a1bb85359131f075c0c67195c7c2ac64b0274b
Yu <fyu3228@gmail.com>
2025-02-24 12:48:11 -0600

    update login

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development', // 可选 'production' 或 'development'
-  entry: './src/index.js', // 入口文件
+  mode: 'development',
+  entry: './src/index.js',
+  homepage: "https://anfjbajfa.github.io/CityWalkerApp/",
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -10,18 +11,18 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/, // 匹配所有 .js 文件
-        exclude: /node_modules/, // 排除 node_modules 文件夹
+        test: /\.js$/, 
+        exclude: /node_modules/, 
         use: {
-          loader: 'babel-loader', // 使用 babel-loader 转换代码
+          loader: 'babel-loader', 
         },
       },
     ],
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'), // 告诉开发服务器从哪个目录提供内容
-    hot: true, // 启用热更新
-    compress: true, // 启用 gzip 压缩
-    port: 3000, // 开发服务器端口号
+    contentBase: path.join(__dirname, 'dist'), 
+    hot: true, 
+    compress: true, 
+    port: 3000,
   },
 };
